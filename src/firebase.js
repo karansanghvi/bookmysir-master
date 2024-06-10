d4702e67205e12ee60b22de355879f019619483e
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-06-10 16:27:58 +0530

    login

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; 
-import { getAuth } from "firebase/auth"; // Import Firebase Authentication
+import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyAkKpmKWKhh9-MNLADPs1NirNwvRziy0lU",
+  authDomain: "bookmysir-eb349.firebaseapp.com",
+  projectId: "bookmysir-eb349",
+  storageBucket: "bookmysir-eb349.appspot.com",
+  messagingSenderId: "598724369042",
+  appId: "1:598724369042:web:143eb81005fba4f587a44b"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Initialize Firestore
-const auth = getAuth(app); // Initialize Firebase Authentication
+let app;
+let db;
+let auth;
 
-export { app, db, auth }; // Export app, db, and auth for use in other files
+// Initialize Firebase
+try {
+  app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
+  auth = getAuth(app);
+  console.log("Firebase connected");
+} catch (error) {
+  console.error("Firebase not connected:", error);
+}
+
+export { app, db, auth };
